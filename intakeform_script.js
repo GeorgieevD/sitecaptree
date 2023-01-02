0c9207c2a8e22dc56f2434d5cb81a85f71df423d
kvv-nl <80898178+kvv-nl@users.noreply.github.com>
2023-01-02 13:04:03 +0000

    Create intakeform_script.js

---
+++ intakeform_script.js
@@ -0,0 +1,54 @@
+
+	
+var page_number = 0;
+/* page 1 elements */
+
+const raise_wrapper = document.getElementById("raise_wrapper")
+const funding_timing = document.getElementById("funding_timing_block")
+funding_timing.style.visibility="hidden"
+UpdatePGHeight(0) 
+
+/* page 2 elements */
+const p2_first_block = document.getElementById("p2_first_block")
+
+/* page 3 elements */
+
+const grid_wapper_p3 = document.getElementById("grid_wrapper_p3")
+
+/* page 4 elements */
+const year_wrapper = document.getElementById("year_wrapper")
+const company_size_wrapper = document.getElementById("company_size_wrapper")
+const ct_other_button = document.getElementById("company_type_other")
+const ct_other_div = document.getElementById("company_type_other_div")
+company_size_wrapper.style.visibility="hidden"
+ct_other_div.style.visibility="hidden"
+
+/* page 5 elements */
+const p5_first_block = document.getElementById("p5_first_block")
+const rev_growth_wrapper = document.getElementById("rev_growth_wrapper")
+const rev_recurring = document.getElementById("rev_recurring")
+rev_growth_wrapper.style.visibility="hidden"
+rev_recurring.style.visibility="hidden"
+
+/* page 6 elements */
+const p6_first_block = document.getElementById("p6_first_block")
+const EBITDA_wrapper = document.getElementById("EBITDA_wrapper")
+const burn_rate_wrapper = document.getElementById("burn_rate_wrapper")
+const prof_yes_button = document.getElementById("prof_yes_button")
+EBITDA_wrapper.style.display="none"
+burn_rate_wrapper.style.visibility="hidden"
+
+/* page 7elements */
+const p7_first_block = document.getElementById("p7_first_block")
+const equity_wrapper = document.getElementById("equity_wrapper")
+const cap_wrapper = document.getElementById("cap_wrapper")
+cap_wrapper.style.visibility="hidden"
+equity_wrapper.style.visibility="hidden"
+
+
+/* page 8 elements */
+const p8_first_block = document.getElementById("p8_first_block")
+const equity_wrapper = document.getElementById("equity_wrapper")
+const cap_wrapper = document.getElementById("cap_wrapper")
+cap_wrapper.style.visibility="hidden"
+equity_wrapper.style.visibility="hidden"
