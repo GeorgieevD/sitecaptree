24f8cefadf44378b28fb2755e86aa8127fccc05f
kvv-nl <80898178+kvv-nl@users.noreply.github.com>
2023-01-05 00:01:17 +0000

    Update general_functions5.js

--- general_functions5.js
+++ general_functions5.js
@@ -7,9 +7,7 @@ function setCookie(cookieName, cookieValue, nDays) {
 
     if (!nDays) 
         nDays=default_cookie_exp;
-  console.log('cookieName: ' + cookieName)
-  console.log('cookieValue: ' + cookieValue)
-  console.log('nDays: ' + nDays)
+
     expire.setTime(today.getTime() + 3600000*24*nDays);
      document.cookie = cookieName+"="+escape(cookieValue) + ";expires="+expire.toGMTString();
     return false
