3385a1b8641545a7c20f0de4c5454d8c7fcd7327
kvv-nl <80898178+kvv-nl@users.noreply.github.com>
2023-05-02 14:37:15 +0200

    Update and rename hansworst_scriptv87.js to hansworst_scriptv88.js

--- hansworst_scriptv88.js
+++ hansworst_scriptv88.js
@@ -333,13 +333,16 @@ function createCopiesFromDict(inputDict) {
       } else if (oldId === "quantum_accolade") {
         	element.style.display = 'none'
 		if (inputDict[productName].quantum_met){
-	  		element.style.display = "flex"} 		      
+	  		element.style.display = "flex"
+		        element.style.zIndex = "0"} 		      
       } else if (oldId === "rate_accolade") {
         	element.style.display = 'none'
+	        element.style.zIndex = "0" 
 	        if (findLowestRateRow(inputDict) === productName){
 	      		element.style.display = "flex"} 		      
       } else if (oldId === "ranking_accolade") {
         	element.style.display = 'none'
+	        element.style.zIndex = "0"
 	        if (inputDict[productName].ranking == 'junior'){
 	      		element.style.display = "flex"}
       } else if (oldId === "product_quantum") {
